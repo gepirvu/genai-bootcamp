1fac51cb66d9fa191371db151dda8605ffc1485a
George Pirvu <46958544+gepirvu@users.noreply.github.com>
2025-08-05 01:35:41 +0200

    Update app.py

--- week8/1-admin-ui/app.py
+++ week8/1-admin-ui/app.py
@@ -14,6 +14,8 @@ _ = Twin(app, "Twin",
          kb_id=kb.kb.knowledge_base_id,
          kb_data_src_id=kb.kb.data_source_id,
          kb_input_bucket=kb.input_bucket,
+         custom_certificate_arn="arn:aws:acm:us-east-1:950363886497:certificate/83f17796-48de-4a8f-b027-6bd25e2ac957",
+         custom_domain_name="twin.tech-george.com"
          env=env,
         )
 
