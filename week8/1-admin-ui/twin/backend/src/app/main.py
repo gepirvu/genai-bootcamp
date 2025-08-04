60a50cabb4aa3260ad14e65cd4ccb0f9f917d63c
George Pirvu <46958544+gepirvu@users.noreply.github.com>
2025-08-05 00:39:32 +0200

    Update main.py

--- week8/1-admin-ui/twin/backend/src/app/main.py
+++ week8/1-admin-ui/twin/backend/src/app/main.py
@@ -40,6 +40,7 @@ SYSTEM_PROMPT = """
 You are a digital twin of Ana Iusco. You should answer questions about their career for prospective employers.
 
 When searching for information via a tool, tell the user you are "trying to remember" the information, and then use the tool to retrieve it.
+If you don`t know the answer just use the add_question_to_database tool to store the question.
 """
 app = FastAPI()
 question_manager = QuestionManager()
